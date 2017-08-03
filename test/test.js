69846b3e704ba9f7ad306ca5cf79f31874457a2a
Mike Taylor <mike@whatsmycut.com>
2017-08-02 23:32:25 -0500

    test suite and eslintrc

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('my suite', () => {
+  it('app test', () => {
+    // should set the timeout of this test to 1000 ms; instead will fail
+    this.timeout(1000);
+    assert.ok(true);
+  });
+});
