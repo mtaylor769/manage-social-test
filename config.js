50da7f9a6197a543903c5e0ccb2f334acd22f8db
Mike Taylor <mike@whatsmycut.com>
2017-08-02 23:34:14 -0500

    removing errant linebreak

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	consumer_key:         'kAFu2MECWQDOVGmdZPdDlzRfb',
  	consumer_secret:      'UIyKXBYBPcPeRHZ2yqyXrlVxUx83xqPv8FTv08siQ7B51gK4RO',
- 	access_token:         '18629180-mihgqPyVruTYIWaZKlzCTztg9jh3sChaoUfXUptkX
-',
+ 	access_token:         '18629180-mihgqPyVruTYIWaZKlzCTztg9jh3sChaoUfXUptkX',
  	access_token_secret:  'f0xuEkNojZjN628DoBT7qjetA6X4rKAxOs2031oR5kqBW'
 }
