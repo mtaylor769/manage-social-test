3bd88c569347ec77900d819bb36c6e8dd3b49fc7
Mike Taylor <mike@whatsmycut.com>
2017-08-02 23:34:42 -0500

    route stubs

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
-const twitter = require('twitter-api')
+const twitter = require('twit')
 const app = express()
+app.locals.strftime = require('strftime')
+app.locals.title = 'ManageSocial Test'
+app.locals.email = 'mike@whatsmycut.com'
+
+var assert = require('chai').assert;
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+})
+
+app.get('/oauth_request', function (req, res) {
+  res.send('oauth request!')
+})
+
+app.post('/connect', function(req, res){
+  res.send('connect!')
+})
+
+app.get('/tweets', function(req, res){
+  res.send('tweets!')
+})
+
+app.post('/disconnect', function(req, res){
+  res.send('disconnect!')
 })
 
 app.listen(3000, function () {
