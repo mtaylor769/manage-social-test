e44a5041b47d00f1cad6ae4c67c5ad2a67f0eb6e
Mike Taylor <mike@whatsmycut.com>
2017-08-07 02:20:34 -0500

    oauth pipeline working

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const request = require("request")
+const _ = require("lodash")
+const json = require('json')
 const Twit = require("twit")
 const app = express()
 app.locals.strftime = require("strftime")
@@ -8,6 +10,7 @@ app.locals.email = "mike@whatsmycut.com"
 
 const config = require("./config")
 const T = new Twit(config)
+let tcreds = []
 
 app.get("/", function (req, res) {
   res.type("application/json")
@@ -28,21 +31,56 @@ app.get("/", function (req, res) {
 
 app.get("/oauth_request", function (req, res) {
   res.type("text/plain")
-  T.get("account/verify_credentials", { skip_status: true })
+  T.post("https://api.twitter.com/oauth/request_token", 
+    { 
+      skip_status: true, 
+      oauth_callback:"http%3A%2F%2Flocalhost%3A8080%2Fsign-in-with-twitter%2F",
+      oauth_consumer_key: config.consumer_key,
+      oauth_nonce:"ea9ec8429b68d6b77cd5600adbbb0456",
+      oauth_signature:config.app_only_auth,
+      oauth_signature_method:"HMAC-SHA1",
+      oauth_timestamp:Date.now(),
+      oauth_version:"1.0"
+    })
   .catch(function (err) {
     console.log("caught error", err.stack)
   })
   .then(function (result) {
-    console.log("data", result.data);
+    let pairs = result.data.split('&')
+    let vals = []
+    _.each(pairs, function(t) {
+      let sp = t.split('=')
+      vals[sp[0]] = sp[1]
+    })
+    tcreds = vals
+    res.status(302).redirect("/sign-in-with-twitter")
   })
-  res.send(data)
+  
+})
+
+app.get("/sign-in-with-twitter", function(req, res){
+  console.log("/sign-in-with-twitter called", req.query)
+  if (undefined === req.query['oauth_verifier']) {
+    T.get('https://api.twitter.com/oauth/authenticate', {'oauth_token': tcreds['oauth_token']})
+    .catch(function (err) {
+      console.log("caught error", err.stack)
+    })
+    .then(function (result) {
+      res.send(result.data)
+    })
+  } else {
+    tcreds.concat(res.query)
+    res.status(200).redirect('/tweets')
+  }
 })
 
 app.post("/connect", function(req, res){
+  console.log("connect: tcreds", tcreds)
   res.send("connect!")
 })
 
 app.get("/tweets", function(req, res){
+  console.log("tweets: tcreds", tcreds)
   res.send("tweets!")
 })
 
